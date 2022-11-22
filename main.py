6af08e35e9f962ed5469b5ccbb243e9cf0a7373b
SenaGit <tosenadha@gmail.com>
2022-11-22 19:09:54 +0530

    New code

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
+
+
+print(f"chars: {chars}")
+print(f"key: {key}")
