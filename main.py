057ae5cbd1e499bb3542013e179da3f04b4ad858
SenaGit <tosenadha@gmail.com>
2022-11-25 19:40:08 +0530

    New Code addded

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ chars = " " + string.punctuation + string.digits + string.ascii_letters
 chars = list(chars)
 key = chars.copy()
 
+random.shuffle(key)
 
-print(f"chars: {chars}")
-print(f"key: {key}")
+# print(f"chars: {chars}")
+# print(f"key: {key}")
+
+plain_text = input("Enter a message encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"Original message: {plain_text}")
+print(f"Encrypted message: {cipher_text}")
